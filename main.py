4f2253b9514e49874580b1ecec3706c0cd7bf284
cowboy8625 <cowboy8625@yahoo.com>
2019-11-02 17:09:16 -0500

    added unique word count

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-filename = "KJV.txt"
+import time
 
 def get_file(filename):
     with open(filename) as f:
         for line in f.readlines():
             yield line[0:-1]
-
-
-data = get_file(filename)
 
 def get_word_count(data):
     count = 0
@@ -14,4 +11,36 @@ def get_word_count(data):
         word = line.replace("\t", " ").split(" ")
         count += len(word)
     return count
-print(get_word_count(data))
+
+def get_line_list(line):
+    return line.replace("\t", " ").split(" ")
+
+def remove_char(word):
+    result = ''
+    for ch in word:
+        if ch not in (",.:;!?~ "):
+            result += ch
+    return result
+
+def get_unique_count(data):
+    word_count = {}
+    for line in data:
+        for word in get_line_list(line):
+            word = remove_char(word.lower())
+            if word.isdigit():
+                continue
+            elif word not in word_count.keys():
+                word_count[word] = 1
+            else:
+                word_count[word] += 1
+    return word_count
+
+start = time.time()
+filename = "KJV.txt"
+data = get_file(filename)
+# print(get_word_count(data))
+new_data = get_unique_count(data)
+end = time.time()
+time_taken = end - start
+print(new_data)
+print(time_taken)
